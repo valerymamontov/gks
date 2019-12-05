2067bcfc55b2c82ae0d56a371af9808f88cba46a
Valery Mamontov <genius2003klg@mail.ru>
2019-12-05 11:18:21 +0300

    add files and folders in .gitignore, edit script.py

--- script.py
+++ script.py
@@ -3,28 +3,27 @@ import xml.dom.minidom
 dom = xml.dom.minidom.parse("data.xml")
 dom.normalize()
 
+# -------------- формирование списка регионов --------------
+code_list = ['643', '030', '14000000000', '15000000000', '17000000000', '20000000000', '24000000000',
+             '29000000000', '34000000000', '38000000000', '42000000000', '46000000000', '54000000000',
+             '61000000000', '66000000000', '68000000000', '28000000000', '70000000000', '78000000000', '45000000000']
 
-
-# -------------- формирование списка регионов
 dictRegions = {}
 for i in dom.getElementsByTagName("structure:CodeList")[0].getElementsByTagName("structure:Code"):
-    if i.getAttribute("value") in ['643', '030', '14000000000', '15000000000', '17000000000', '20000000000',
-                                   '24000000000', '29000000000', '34000000000', '38000000000', '42000000000',
-                                   '46000000000', '54000000000', '61000000000', '66000000000', '68000000000',
-                                   '28000000000', '70000000000', '78000000000', '45000000000']:
-        if i.getAttribute("value") == '45000000000':
-            dictTemp = {"%s" % i.getAttribute("value"):"Город Москва"}
+    code = str(i.getAttribute("value"))
+    if code in code_list:
+        if code is '45000000000':
+            dictRegions[code] = "Город Москва"
         else:
-            dictTemp = {"%s" % i.getAttribute("value"):"%s" % i.getElementsByTagName("structure:Description")[0].firstChild.data}
-        dictRegions.update(dictTemp)
+            dictRegions[code] = str(i.getElementsByTagName("structure:Description")[0].firstChild.data)
 
 
-# -------------- открытие текстового файл и запись в него заголовка
+# -------------- открытие текстового файл и запись в него заголовка --------------
 file = open("data.csv", "a")
 file.write("Регион;Структура населения;Тип поселения;%s" % ';'.join(str(e) for e in range(1990, 2018)))
 
 
-# -------------- создание всех возможных наборов данных
+# -------------- создание всех возможных наборов данных --------------
 # 1.всё население, Оба пола           2. всё население, Женщины           3. всё население, Мужчины
 # 4.городское население, Оба пола     5. городское население, Женщины     6. городское население, Мужчины
 # 7.сельское население, Оба пола      8. сельское население, Женщины      9. сельское население, Мужчины
@@ -34,21 +33,29 @@ sl = [["w2:p_mest:11", "12"], ["w2:p_mest:11", "2"], ["w2:p_mest:11", "3"],
       ["w2:p_mest:13", "11"], ["w2:p_mest:13", "2"], ["w2:p_mest:13", "3"]]
 
 
-# -------------- последовательный перебор всех наборов данных
+# -------------- последовательный перебор всех наборов данных --------------
 for m in range(len(sl)):
 
     # -------------- условия --------------
-    if sl[m][0] == "w2:p_mest:11": population = "всё население"
-    elif sl[m][0] == "w2:p_mest:12": population = "городское население"
-    elif sl[m][0] == "w2:p_mest:13": population = "сельское население"
-    else: population = ""
+    if sl[m][0] == "w2:p_mest:11":
+        population = "всё население"
+    elif sl[m][0] == "w2:p_mest:12":
+        population = "городское население"
+    elif sl[m][0] == "w2:p_mest:13":
+        population = "сельское население"
+    else:
+        population = ""
 
-    if sl[m][1] == "12": gender = "Оба пола"
-    elif sl[m][1] == "2": gender = "Женщины"
-    elif sl[m][1] == "3": gender = "Мужчины"
-    else: gender = ""
+    if sl[m][1] == "12":
+        gender = "Оба пола"
+    elif sl[m][1] == "2":
+        gender = "Женщины"
+    elif sl[m][1] == "3":
+        gender = "Мужчины"
+    else:
+        gender = ""
 
-    # -------------- перебор всех Регионов
+    # -------------- перебор всех Регионов --------------
     for key in dictRegions.keys():
         genSeries = dom.getElementsByTagName("generic:Series")
 
@@ -61,16 +68,15 @@ for m in range(len(sl)):
 
             if codeOrg == key and codeGender == sl[m][1] and codePopulation == sl[m][0]:
                 if len(oneString) == 0:
-                    oneString = "\n%s;%s;%s;%s" % (
-                        dictRegions[key],
-                        gender,
-                        population,
-                        g.getElementsByTagName("generic:ObsValue")[0].getAttribute("value"))
+                    oneString = "\n{a};{b};{c};{d}".format(
+                        a=dictRegions[key],
+                        b=gender,
+                        c=population,
+                        d=g.getElementsByTagName("generic:ObsValue")[0].getAttribute("value"))
                 else:
-                    oneString += ";%s" % g.getElementsByTagName("generic:ObsValue")[0].getAttribute("value")
+                    oneString += ";{}".format(g.getElementsByTagName("generic:ObsValue")[0].getAttribute("value"))
 
-        # -------------- запись строки с данными по одному Региону в файл
+        # -------------- запись строки с данными по одному Региону в файл --------------
         file.write(oneString)
-
 
 file.close()
